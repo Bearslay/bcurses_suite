2ba3ef6b7986d17729570d762689aee49377c90f
Bearslay <barfunkel@protonmail.com>
2025-01-23 10:12:32 -0500

    bengine_curses system development going well

--- src/test.cpp
+++ src/test.cpp
@@ -12,7 +12,8 @@ class test : public bengine::curses_loop {
                     this->loop_running = false;
                     break;
                 case 'a':
-                    this->window.write_character(repeats, 0, L'a');
+                    this->window.write_character(repeats, 0, L'a', bengine::curses_window::preset_colors::RED, bengine::curses_window::cell_attributes::BOLD);
+                    this->window.write_character(0, 1, this->window.check_cell_attribute_states(0, 0, bengine::curses_window::cell_attributes::BOLD | bengine::curses_window::cell_attributes::ITALIC) ? L'1' : L'0');
                     repeats++;
                     this->visuals_changed = true;
                     break;
@@ -20,7 +21,7 @@ class test : public bengine::curses_loop {
         }
         void compute() {}
         void render() {
-            this->window.present_window();
+            this->window.apply_to_screen();
         }
 
     public:
